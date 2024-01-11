8f7a0023a53531a724a9406c7567533c059c8a6e
nadya-lynn <154042485+nadya-lynn@users.noreply.github.com>
2024-01-11 16:04:20 -0600

    checking in candidate testing autograded

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question ="Who was the first American woman in space?";
+let correctAnswer= "Sally Ride";
+let candidateAnswer= "";
 
 
 //TODO: Variables for Part 2
@@ -18,13 +18,15 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName = input.question("Please enter your name:    ");
+  let candidateName= prompt;
+  console.log("Hello, " + candidateName  + "!");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  question  = input.question("Who was the first American woman in space?");
+  let question = prompt;
 }
 
 function gradeQuiz(candidateAnswers) {

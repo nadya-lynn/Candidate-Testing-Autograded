1cdfa75ff2a3482fde5a0945e0902d37d68fecfc
nadya-lynn <154042485+nadya-lynn@users.noreply.github.com>
2024-01-20 21:12:46 -0600

    checking in Candidate-Testing-Autograded Part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,31 +3,40 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = "";
+let candidateName = " ";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question ="Who was the first American woman in space? ";
 let correctAnswer= "Sally Ride";
-let candidateAnswer= "";
+let candidateAnswer= " ";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? " , 
+"True or false: 5 kilometer == 5000 meters? " , 
+ "(5 + 3)/2 * 10 = ? ", 
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
+"What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = " ";
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName = input.question("Please enter your name:    ");
+  candidateName = input.question(`Please enter your name:    `);
   let candidateName= prompt;
-  console.log("Hello, " + candidateName  + "!");
+  console.log(`Hello,  + ${candidateName}  + !`);
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  question  = input.question("Who was the first American woman in space?");
-  let question = prompt;
+  question  = input.question(" ");
+  let question = prompt
+  for (let i = 0; i < questions.length; i++) {
+    console.log(questions[i]);
+  } 
 }
+
+
 
 function gradeQuiz(candidateAnswers) {
 

4f0173381b7a62efcdbf7fc23df974872b896e10
nadya-lynn <154042485+nadya-lynn@users.noreply.github.com>
2024-01-21 13:48:23 -0600

    checking in Candidate-Testing_Autograded Part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -13,11 +13,11 @@ let candidateAnswer= " ";
 //TODO: Variables for Part 2
 let questions = ["Who was the first American woman in space? " , 
 "True or false: 5 kilometer == 5000 meters? " , 
- "(5 + 3)/2 * 10 = ? ", 
+"(5 + 3)/2 * 10 = ? ", 
 "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
 "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
-let candidateAnswers = " ";
+let candidateAnswers = [" "];
 
 
 function askForName() {
@@ -34,7 +34,10 @@ function askQuestion() {
  
   for (let i = 0; i < questions.length; i++) {
     console.log(questions[i]);
+    candidateAnswers = input.question(`Your Answer: `);
     
+     candidateAnswers = [ ];
+     console.log(`Correct Answer: ${correctAnswers[i]}`);
   } 
 }
 
@@ -43,6 +46,18 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+
+  if (candidateAnswers === correctAnswers.length) {
+    console.log(` You are correct`);
+    candidateAnswers = " ";
+  } if 
+    (candidateAnswers !== correctAnswers) {
+    console.log(`Try Again!`);
+    candidateAnswers = " ";
+  }
+  
+
+  console.log(` ${candidateAnswers}  Correct Answers: ${correctAnswers}`);
 
 
 
